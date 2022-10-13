17836cba6c1149d4d468fce487acb92a590a2eb4
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-10-13 11:44:02 -0400

    Gather components to be exported to app.js

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+// This is a cleaner way to import multiple files to app.js
+
+export { default as Feed } from "./Feed";
+export { default as Navbar } from "./Navbar";
+export { default as SearchFeed } from "./SearchFeed";
+export { default as VideoDetail } from "./VideoDetail";
+export { default as ChannelDetail } from "./ChannelDetail";
+export { default as Searchbar } from "./SearchBar";
+
