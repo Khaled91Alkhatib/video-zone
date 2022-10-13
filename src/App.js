74f21e6aa96371650d970919ad634ae79fb07152
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-10-13 11:40:45 -0400

    Implement routes for website and import necessary components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material"; // Box is like a div element
 
-function App() {
-  return (
-    <div>
-      Khaled
-    </div>
-  );
-}
+import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from "./components";
+
+// Removing the return word and the paranthesis makes it an instant return
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: "#000" }}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Feed />} />
+        <Route path="/video/:id" element={<VideoDetail />} />
+        <Route path="/channel/:id" element={<ChannelDetail />} />
+        <Route path="/search/:searchTerm" element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>
+);
 
 export default App;
