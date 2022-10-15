7bb99d09daaa3463d0752c8d24240c67cbcc1d49
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-10-15 09:24:31 -0400

    Show the results of the submit bar from the axios request

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Box, Typography } from '@mui/material';
+import { Videos } from '../components';
+import { fetchFromAPI } from '../utilities/fetchFromAPI';
+import { useParams } from 'react-router-dom';
 
 const SearchFeed = () => {
-  return (
-    <div>SearchFeed</div>
-  )
-}
+  const [videos, setVideos] = useState([]);
+  const { searchTerm } = useParams();
 
-export default SearchFeed
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`)
+      .then((data) => {
+        // console.log(data)
+        setVideos(data.items);
+      });
+  }, [searchTerm]);
+
+  return (
+    <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }} display="flex" flexDirection="column" alignItems="center" >
+      <Typography variant='h4' fontWeight="bold" mb={2} sx={{ color: "white" }} >
+        Search results for
+        <span style={{ color: "#FC1503" }}>
+          &nbsp;{searchTerm}
+        </span> videos
+      </Typography>
+      <Videos videos={videos} />
+    </Box>
+  );
+};
+
+export default SearchFeed;
