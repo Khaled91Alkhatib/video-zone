d3bd84a5eb434cac741b4302e85262dc4c183b6e
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-10-15 14:44:11 -0400

    Modify the site logo and name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import { Stack } from '@mui/material';
 import { Link } from 'react-router-dom';
 
-import { logo } from '../utilities/constants';
 import SearchBar from './SearchBar';
 import "../styles/Navbar.scss";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFilm } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
   return (
     <Stack className='navbar-stack' direction="row">
       <Link to="/" className='navbar-link'>
-        <img src={logo} alt="logo" className='logo' />
+        <div className="app-name">Video<FontAwesomeIcon icon={faFilm} className="icon" />Zone</div>
       </Link>
       <SearchBar />
     </Stack>
